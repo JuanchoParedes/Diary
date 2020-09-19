8a448e42214cba9c88a06daf7982af9e26c6c2b2
Juan Paredes <juanfranciscop@gmail.com>
2020-09-19 12:36:48 -0500

    changed picture format

--- FacebookLoginWithGooglePlaces/app/src/main/java/com/sunchildapps/facebookloginwithgoogleplaces/MainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.sunchildapps.facebookloginwithgoogleplaces
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-}
--- app/src/main/java/com/sunchildapps/diary/model/DiaryEntry.kt
+++ app/src/main/java/com/sunchildapps/diary/model/DiaryEntry.kt
@@ -6,7 +6,7 @@ import android.location.Location
 data class DiaryEntry(
     val id: Int,
     val entryTitle: String,
-    var picture: BitmapDrawable? = null,
+    var pictureUrl: String? = null,
     var entryDescription: String,
     val location: Location? = null
 
